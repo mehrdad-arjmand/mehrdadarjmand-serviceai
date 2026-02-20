139d2416b0302b04419e0efc4c0e729e42090c54
mehrdad-arjmand <mehrdad.arjmand@gmail.com>
2026-02-20 02:58:03 +0000

    Changes

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -80,7 +80,7 @@ export default function Settings() {
           Back
         </Button>
 
-        <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-16">
           {/* Account Information */}
           <Card>
             <CardHeader>
